927cd7f3b8ff9a9a467fe2447566682ee3cb0175
HK-hub <3161880795@qq.com>
2023-01-24 00:39:40 +0800

    start the gruop announcement

---
+++ im-domain/src/main/java/com/hk/im/domain/request/GroupAnnouncementRequest.java
@@ -0,0 +1,14 @@
+package com.hk.im.domain.request;
+
+/**
+ * @author : HK意境
+ * @ClassName : GroupAnnouncementRequest
+ * @date : 2023/1/23 18:22
+ * @description :
+ * @Todo :
+ * @Bug :
+ * @Modified :
+ * @Version : 1.0
+ */
+public class GroupAnnouncementRequest {
+}
---
+++ im-web/im-admin/src/main/java/com/hk/im/admin/controller/GroupAnnouncementController.java
@@ -0,0 +1,47 @@
+package com.hk.im.admin.controller;
+
+import com.hk.im.common.resp.ResponseResult;
+import com.hk.im.domain.entity.GroupAnnouncement;
+import com.hk.im.domain.request.GroupAnnouncementRequest;
+import com.hk.im.service.service.GroupAnnouncementService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author : HK意境
+ * @ClassName : GroupAnnouncementController
+ * @date : 2023/1/23 18:18
+ * @description :
+ * @Todo :
+ * @Bug :
+ * @Modified :
+ * @Version : 1.0
+ */
+@Slf4j
+@CrossOrigin
+@RestController
+@RequestMapping("/announcement")
+public class GroupAnnouncementController {
+
+    @Resource
+    private GroupAnnouncementService groupAnnouncementService;
+
+
+    /**
+     * 发布群公告
+     * @param request
+     * @return
+     */
+    @PostMapping("/publish")
+    public ResponseResult publishAnnouncement(GroupAnnouncementRequest request) {
+        return null;
+    }
+
+
+
+}
